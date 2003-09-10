a88913b9740f2ab7962d026f2a30d3c5ed82355f
Max Horn <max@quendi.de>
2003-09-10 21:39:10 +0000

    added symbolic var names for V3,4,5
    svn-id: r270

--- descumm.cpp
+++ descumm.cpp
@@ -160,6 +160,70 @@ const char *var_names2[] = {
 	NULL
 };
 
+const char *var_names345[] = {
+	/* 0 */
+	"VAR_KEYPRESS",
+	"VAR_EGO",
+	"VAR_CAMERA_POS_X",
+	"VAR_HAVE_MSG",
+	/* 4 */
+	"VAR_ROOM",
+	"VAR_OVERRIDE",
+	"VAR_MACHINE_SPEED",
+	"VAR_ME",
+	/* 8 */
+	"VAR_NUM_ACTOR",
+	"VAR_CURRENT_LIGHTS",
+	"VAR_CURRENTDRIVE",
+	"VAR_TMR_1",
+	/* 12 */
+	"VAR_TMR_2",
+	"VAR_TMR_3",
+	"VAR_MUSIC_TIMER",
+	"VAR_ACTOR_RANGE_MIN",
+	/* 16 */
+	"VAR_ACTOR_RANGE_MAX",
+	"VAR_CAMERA_MIN_X",
+	"VAR_CAMERA_MAX_X",
+	"VAR_TIMER_NEXT",
+	/* 20 */
+	"VAR_VIRT_MOUSE_X",
+	"VAR_VIRT_MOUSE_Y",
+	"VAR_ROOM_RESOURCE",
+	"VAR_LAST_SOUND",
+	/* 24 */
+	"VAR_CUTSCENEEXIT_KEY",
+	"VAR_TALK_ACTOR",
+	"VAR_CAMERA_FAST_X",
+	"VAR_SCROLL_SCRIPT",
+	/* 28 */
+	"VAR_ENTRY_SCRIPT",
+	"VAR_ENTRY_SCRIPT2",
+	"VAR_EXIT_SCRIPT",
+	"VAR_EXIT_SCRIPT2",
+	/* 32 */
+	"VAR_VERB_SCRIPT",
+	"VAR_SENTENCE_SCRIPT",
+	"VAR_INVENTORY_SCRIPT",
+	"VAR_CUTSCENE_START_SCRIPT",
+	/* 36 */
+	"VAR_CUTSCENE_END_SCRIPT",
+	"VAR_CHARINC",
+	"VAR_WALKTO_OBJ",
+	NULL,
+	/* 40 */
+	NULL,
+	NULL,
+	"VAR_RESTART_KEY",
+	"VAR_PAUSE_KEY",
+	/* 44 */
+	"VAR_MOUSE_X",
+	"VAR_MOUSE_Y",
+	"VAR_TIMER",
+	"VAR_TMR_4"
+};
+
+
 const char *get_num_string(int i)
 {
 	const char *s;
@@ -202,8 +266,13 @@ char *get_var(char *buf)
 	else
 		i = get_word();
 
-	if (scriptVersion <= 2 && i < ARRAYSIZE(var_names2) && var_names2[i]) {
+	if (scriptVersion <= 2 &&
+			i < ARRAYSIZE(var_names2) && var_names2[i]) {
 		buf += sprintf(buf, var_names2[i]);
+		return buf;
+	} else if (scriptVersion >= 3 && scriptVersion <= 5 && 
+			i < ARRAYSIZE(var_names345) && var_names345[i]) {
+		buf += sprintf(buf, var_names345[i]);
 		return buf;
 	} else if ((i & 0x8000) && (GF_UNBLOCKED || ZakFlag))
 		buf += sprintf(buf, "Var[%d Bit %d", (i & 0x0FFF) >> 4, i & 0x000F);
