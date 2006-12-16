ef4301de7841090fa181bbf5e73f6fbcc294c86b
Eugene Sandulenko <sev@scummvm.org>
2006-12-16 05:28:01 +0000

    Add batches for autoconverting Feeble Files SMK videos.
    svn-id: r24859

---
+++ convert_dxa_one.bat
@@ -0,0 +1,34 @@
+rem Auxiliary script for converting Feeble Files video to ScummVM format
+rem
+rem (c) 2006 ScummVM Team
+rem (c) 2006 oduverne
+rem
+rem $URL$
+rem
+rem $Id$
+
+@echo off
+
+if "%DXA_CONVERT%"=="CORRECT" goto proceed
+
+goto usage
+
+:proceed
+
+echo Processing %1...
+"%BINK_PATH%\BinkConv.exe" %1 "%SMK_PATH%\%~n1.png" /n-1 /z1 /#
+"%BINK_PATH%\BinkConv.exe" %1 "%SMK_PATH%\%~n1.wav" /v /#
+cd "%SMK_PATH%"
+"%TOOLS_PATH%\encode_dxa.exe" --vorbis "%~n1.smk"
+echo Deleting temp files
+del "%SMK_PATH%\%~n1*.png"
+del "%SMK_PATH%\%~n1.wav"
+move "%SMK_PATH%\%~n1.*" "%DXA_PATH%"
+cd "%TOOLS_PATH%"
+
+goto quit
+
+:usage
+echo Please, modify and run convert_dxa.bat
+
+:quit
