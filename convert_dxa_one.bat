d5dd61641f11192b04d7d349f111c59692bea194
Travis Howell <kirben@scummvm.org>
2007-09-02 12:46:17 +0000

    Update convert_dxa bat files, to handle different drive letters.
    svn-id: r28817

--- convert_dxa_one.bat
+++ convert_dxa_one.bat
@@ -18,13 +18,13 @@ goto usage
 echo Processing %1...
 "%BINK_PATH%\BinkConv.exe" %1 "%SMK_PATH%\%~n1.png" /n-1 /z1 /#
 "%BINK_PATH%\BinkConv.exe" %1 "%SMK_PATH%\%~n1.wav" /v /#
-cd "%SMK_PATH%"
+cd /d "%SMK_PATH%"
 "%TOOLS_PATH%\encode_dxa.exe" --vorbis "%~n1.smk"
 echo Deleting temp files
 del "%SMK_PATH%\%~n1*.png"
 del "%SMK_PATH%\%~n1.wav"
 move "%SMK_PATH%\%~n1.*" "%DXA_PATH%"
-cd "%TOOLS_PATH%"
+cd /d "%TOOLS_PATH%"
 
 goto quit
 
