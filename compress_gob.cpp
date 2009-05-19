f1a8dd478c40b4e743e869a655e5c4a9e0ddc5ef
Sven Hesse <drmccoy@users.sourceforge.net>
2009-05-19 16:06:16 +0000

    Style (sorry, Strangerke :P)
    svn-id: r40728

--- compress_gob.cpp
+++ compress_gob.cpp
@@ -35,12 +35,12 @@ struct Chunk {
 	~Chunk() { delete next; }
 };
 
-Chunk *readChunkConf (FILE *gobconf, uint16 &chunkCount);
-void writeEmptyHeader (FILE *stk, uint16 chunkCount);
-void writeBody (FILE *stk, uint16 chunkcount, Chunk *chunks);
-uint32 writeBodyFile (FILE *stk, FILE *src);
-uint32 writeBodyPackFile (FILE *stk, FILE *src);
-void rewriteHeader (FILE *stk, uint16 chunkCount, Chunk *chunks);
+Chunk *readChunkConf(FILE *gobconf, uint16 &chunkCount);
+void writeEmptyHeader(FILE *stk, uint16 chunkCount);
+void writeBody(FILE *stk, uint16 chunkcount, Chunk *chunks);
+uint32 writeBodyFile(FILE *stk, FILE *src);
+uint32 writeBodyPackFile(FILE *stk, FILE *src);
+void rewriteHeader(FILE *stk, uint16 chunkCount, Chunk *chunks);
 bool checkDico(byte *unpacked, uint32 unpackedIndex, int32 counter, byte *dico, uint16 &pos, uint8 &length);
 
 byte *packData(byte *src, uint32 &size);
@@ -126,7 +126,7 @@ Chunk *readChunkConf(FILE *gobConf, uint16 &chunkCount) {
 	return chunks;
 }
 
-void writeEmptyHeader (FILE *stk, uint16 chunkCount) {
+void writeEmptyHeader(FILE *stk, uint16 chunkCount) {
 	int count;
 
 // Write empty header
@@ -136,7 +136,7 @@ void writeEmptyHeader (FILE *stk, uint16 chunkCount) {
 	return;
 }
 
-void writeBody (FILE *stk, uint16 chunkCount, Chunk *chunks) {
+void writeBody(FILE *stk, uint16 chunkCount, Chunk *chunks) {
 	Chunk *curChunk = chunks;
 	FILE *src;
 	uint32 realSize;
@@ -169,7 +169,7 @@ void writeBody (FILE *stk, uint16 chunkCount, Chunk *chunks) {
 	return;
 }
 
-uint32 writeBodyFile (FILE *stk, FILE *src) {
+uint32 writeBodyFile(FILE *stk, FILE *src) {
 	int count;
 	char buffer[4096];
 	uint32 tmpSize;
@@ -184,7 +184,7 @@ uint32 writeBodyFile (FILE *stk, FILE *src) {
 }
 
 
-void rewriteHeader (FILE *stk, uint16 chunkCount, Chunk *chunks) {
+void rewriteHeader(FILE *stk, uint16 chunkCount, Chunk *chunks) {
 	uint16 i;
 	char buffer[1024];
 	Chunk *curChunk = chunks;
@@ -234,7 +234,7 @@ void rewriteHeader (FILE *stk, uint16 chunkCount, Chunk *chunks) {
 }
 
 // Some LZ77-variant
-uint32 writeBodyPackFile (FILE *stk, FILE *src) {
+uint32 writeBodyPackFile(FILE *stk, FILE *src) {
 	byte dico[4114];
 	byte writeBuffer[17];
 	uint32 counter;
