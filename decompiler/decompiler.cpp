e54687ea359bc522a7ff9b2cf466366858605062
Michal Janeczek <kjdf@users.sourceforge.net>
2009-06-25 11:09:49 +0000

    decompiler: mark all loops ;)
    svn-id: r41862

--- decompiler/decompiler.cpp
+++ decompiler/decompiler.cpp
@@ -58,9 +58,9 @@ int main(int argc, char **argv) {
 		Graph<Block*> &g = cfg._graph;
 		g.markReversePostOrder();
 		g.intervals();
-		g.loopStruct();
 		for (int i = 0; i < vars["derive"].as<int>(); i++)
 			g.extendIntervals();
+		g.loopStruct();
 		cfg.printDot(cout);
 		exit(0);
 	}
