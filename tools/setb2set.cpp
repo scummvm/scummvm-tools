7c5578594398aac636a54d3a0dbe53b0482ca8a7
Joel Teichroeb <joel@teichroeb.net>
2011-04-30 09:04:26 +0800

    Improved setb2set

--- tools/setb2set.cpp
+++ tools/setb2set.cpp
@@ -16,83 +16,184 @@ using namespace std;
 
 typedef int uint32;
 
-enum SectorType{
-	camera,
-	chernobyl,
-	special,
-	walk
+enum SectorType {
+	NoneType = 0,
+	WalkType = 0x1000,
+	FunnelType = 0x1100,
+	CameraType = 0x2000,
+	SpecialType = 0x4000,
+	HotType = 0x8000
 };
 
-enum SectorVisibility{
-	visible
-};
 
 enum LightType{
 	omni,
 	direct
 };
 
-struct Section{
-	const char* buf;
-	float GetFloat()
-	{
-		float retVal = *(float*) buf;
-		buf+=4;
-		return retVal;
-	}
-	int GetInt()
-	{
-		int retVal = *(int*) buf;
-		buf+=4;
-		return retVal;
-	}
-	virtual uint32 load() = 0;
-	virtual string MakeVeryLongString() = 0;
+class Data {
+public:
+	Data(const char *data);
+	float GetFloat();
+	int GetInt();
+	bool GetBool();
+	string GetString(int length);
+	string GetNullTerminatedString();
+	void Skip(int val);
+private:
+	const char *buf;
 };
 
-struct Sector : public Section
+Data::Data(const char *data)
 {
+	buf = data;
+}
+
+float Data::GetFloat()
+{
+	float retVal = *(float *) buf;
+	buf += 4;
+	return retVal;
+}
+
+int Data::GetInt()
+{
+	int retVal = *(int *) buf;
+	buf += 4;
+	return retVal;
+}
+
+bool Data::GetBool()
+{
+	bool retVal = *(bool *) buf;
+	buf += 1;
+	return retVal;
+}
+
+string Data::GetString(int length)
+{
+	//kind of a hack
+	string s = string(buf);
+	buf += length;
+	return s;
+}
+
+string Data::GetNullTerminatedString()
+{
+	string s = string(buf);
+	buf += s.length()+1;
+	return s;
+}
+
+void Data::Skip(int val)
+{
+	buf += val;
+}
+
+struct Section {
+public:
+	Section(Data *data);
+	//virtual uint32 load() = 0;
+	virtual string ToString() = 0;
+protected:
+	Data *data;
+};
+
+Section::Section(Data *data)
+{
+	this->data = data;
+}
+
+class Sector : public Section
+{
+public:
+	Sector(Data *data);
+
+	virtual string ToString();
+private:
 	string name;
 	int ID; // byte;
 	SectorType type;
-	SectorVisibility visibility;
 	float height;
 	int numVertices; // byte;
 	float* vertices; // 3 * numVertices.
-	virtual uint32 load()
-	{
-		numVertices = GetInt();
-		vertices = new float[3*numVertices];
-		for(int i=0; i < numVertices; i++)
-		{
-			vertices[0+3*i]=GetFloat();
-			vertices[1+3*i]=GetFloat();
-			vertices[2+3*i]=GetFloat();
-		}
-		int nameLength = GetInt();
-		name = buf;
-		buf += nameLength;
-		ID = GetInt();
-		//visible = *buf;
-		buf+=1;
-		//type = *buf;
-		buf+=4;
-		int skip = (*(int*)buf); //64;
-		buf+= (skip+1)*4;
-		height = GetFloat();
-		
-		return 12*numVertices + nameLength + skip*4 + 25;
-	}
-	virtual string MakeVeryLongString()
-	{
-		cout << "SECTOR-MAKESTRING";
-		return "";
-	}
+	bool visible;
 };
 
-struct Setup : public Section
+Sector::Sector(Data *data) : Section(data)
 {
+	numVertices = data->GetInt();
+	vertices = new float[3*numVertices];
+	for(int i=0; i < numVertices; i++)
+	{
+		vertices[0+3*i] = data->GetFloat();
+		vertices[1+3*i] = data->GetFloat();
+		vertices[2+3*i] = data->GetFloat();
+	}
+	int nameLength = data->GetInt();
+
+	name = data->GetString(nameLength);
+	ID = data->GetInt();
+	visible = data->GetBool();
+	type = (SectorType)data->GetInt();
+	int skip = data->GetInt();
+	data->Skip(skip*4);
+	height = data->GetFloat();
+}
+
+string Sector::ToString()
+{
+	stringstream ss;
+	ss.precision(6);
+	ss.setf(ios::fixed,ios::floatfield);
+	ss << "\tsector\t" << name << endl;
+	ss << "\tID\t" << ID << endl;
+	ss << "\ttype\t";
+	switch (type) {
+	case WalkType:
+		ss << "walk";
+		break;
+	case FunnelType:
+		ss << "funnel";
+		break;
+	case CameraType:
+		ss << "camera";
+		break;
+	case SpecialType:
+		ss << "special";
+		break;
+	case HotType:
+		ss << "hot";
+		break;
+	};
+	ss << endl;
+	ss << "\tdefault visibility\t";
+	if (visible)
+		ss << "visible";
+	else
+		ss << "invisible";
+	ss << endl;
+	ss << "\theight\t" << height << endl;
+	ss << "\tnumvertices\t" << numVertices << endl;
+	ss << "\tvertices:\t\t";
+	for (int i = 0; i < numVertices*3; i+=3) {
+		if (i != 0)
+			ss << "\t\t\t\t";
+		ss << vertices[i] << "\t" << vertices[i+1] << "\t" << vertices[i+2] << endl;
+	}
+
+	return ss.str();
+}
+
+class Setup : public Section
+{
+public:
+	Setup(Data *data);
+
+	virtual string ToString();
+private:
 	string name;
+	string tile;
 	string background;
 	string zbuffer;
 	float* position;
@@ -101,66 +202,61 @@ struct Setup : public Section
 	float fov;
 	float nclip;
 	float fclip;
-	virtual uint32 load()
-	{
-		cout << "Running setup-load";
-		name = buf; // Parse a string really
-		buf += 128;
-
-		// Skip an unknown number
-		buf += 4;
-
-		char title[128];
-		
-		char in = 0;
-		int i = 0;
-		do {
-			in = *buf;
-			++i;
-			++buf;
-		}while(in);
-
-		position = new float[3];
-		
-		position[0] = GetFloat();
-		position[1] = GetFloat();
-		position[2] = GetFloat();
-		
-		interest = new float[3];
-
-		interest[0] = GetFloat();
-		interest[1] = GetFloat();
-		interest[2] = GetFloat();
-		
-		roll = GetFloat();
-		fov  = GetFloat();
-		nclip = GetFloat();
-		fclip = GetFloat();
-		//	cout << MakeVeryLongString();
-
-		return 172+i;
-	}
-
-	virtual string MakeVeryLongString()
-	{
-		cout << "SETUP-MAKESTRING";
-		stringstream ss;
-		ss << "\tname\t" << name << "\n";
-		// background
-		// zbuffer
-		ss << "\tposition\t" << position[0] << "\t" << position[1] << "\t" << position[2] << "\n";
-		ss << "\tinterest\t" << interest[0] << "\t" << interest[1] << "\t" << interest[2] << "\n";
-		ss << "\troll\t" << roll << "\n";
-		ss << "\fov\t" << fov << "\n";
-		ss << "\tnclip\t" << nclip << "\n";
-		ss << "\tfclip\t" << fclip << "\n";
-
-		return ss.str();
-	}
 };
 
-struct Light : public Section
+Setup::Setup(Data *data) : Section(data)
 {
+	name = data->GetString(128); // Parse a string really
+
+	// Skip an unknown number
+	int unknown = data->GetInt();
+
+
+	tile = data->GetNullTerminatedString();
+
+	position = new float[3];
+
+	position[0] = data->GetFloat();
+	position[1] = data->GetFloat();
+	position[2] = data->GetFloat();
+
+	interest = new float[3];
+
+	interest[0] = data->GetFloat();
+	interest[1] = data->GetFloat();
+	interest[2] = data->GetFloat();
+
+	roll = data->GetFloat();
+	fov  = data->GetFloat();
+	nclip = data->GetFloat();
+	fclip = data->GetFloat();
+}
+
+string Setup::ToString()
+{
+	stringstream ss;
+	ss.precision(6);
+	ss.setf(ios::fixed,ios::floatfield);
+	ss << "\tname\t" << name << endl;
+	// background
+	// zbuffer
+	ss << "\tposition\t" << position[0] << "\t" << position[1] << "\t" << position[2] << endl;
+	ss << "\tinterest\t" << interest[0] << "\t" << interest[1] << "\t" << interest[2] << endl;
+	ss << "\troll\t" << roll << endl;
+	ss << "\tfov\t" << fov << endl;
+	ss << "\tnclip\t" << nclip << endl;
+	ss << "\tfclip\t" << fclip << endl;
+
+	return ss.str();
+}
+
+class Light : public Section
+{
+public:
+	Light(Data *data);
+	virtual string ToString();
+
+private:
 	string name;
 	LightType type;
 	float* position;
@@ -169,103 +265,98 @@ struct Light : public Section
 	float umbraangla;
 	float penumbraangle;
 	int* color; // Byte
-	virtual uint32 load()
-	{
-		return 100; // Skipme
-	}
-	virtual string MakeVeryLongString()
-	{
-		cout << "LIGHT-MAKESTRING";
-		return "";
-	}
+
 };
 
-struct Set{
+Light::Light(Data *data) : Section(data)
+{
+	data->Skip(100);
+}
+
+string Light::ToString()
+{
+	return "";
+}
+
+class Set {
+public:
+	virtual string ToString();
+	Set(Data *data);
+private:
 	string setName;
 	uint32 numSetups;
 	uint32 numColormaps;
 	uint32 numLights;
 	uint32 numSectors;
-	vector<Section*> setups;
+	vector<Section *> setups;
 	vector<string> colormaps;
-	vector<Section*> lights;
-	vector<Section*> sectors;
-	virtual string MakeVeryLongString()
-	{
-		stringstream ss;
-		// colormaps
-		ss << "section: colormaps\n"; // we don't have any.
-		// setups
-		ss << "section: setups\n";
-		vector<Section*>::iterator it;
-		cout << "Size of setups: " << setups.size();
-		for(it = setups.begin();it!=setups.end();++it)
-		{
-			cout << "HELLO";
-			ss << (*it)->MakeVeryLongString();
-		}
-		// lights
-		cout << "Size of lights: " << lights.size();
-		ss << "section: lights\n";
-		for(it = lights.begin();it!=lights.end();it++)
-		{
-			ss << (*it)->MakeVeryLongString();
-		}
-		// sectors
-		cout << "Size of sectors: " << sectors.size();
-		ss << "section: sectors\n";
-		for(it = sectors.begin();it!=sectors.end();it++)
-		{
-			cout << "HELLO";
-			ss << (*it)->MakeVeryLongString();
-		}
-		return ss.str();
-	}
-	Set(const char* buf)
-	{
-		numSetups = *(int*)buf;
-		buf += 4;
-		setups.reserve(numSetups);
-		cout << "We should have " << numSetups << "setups\n";
-		for(int i=0;i<numSetups;i++)
-		{
-			cout << "Adding the " << i << "th Setup\n";
-			setups[i]=new Setup();
-			setups[i]->buf = buf;
-			buf += setups[i]->load();
-		}
-		// Ignore all lights, since we don't know how to handle them
-		numLights = *(int*)buf;
-		buf+=4;
-
-		buf+= numLights*100;
-
-		numSectors = *(int*)buf;
-		buf+=4;
-		sectors.reserve(numSectors);
-		cout << "Numsectors: " << numSectors << endl;
-		for(int i=0;i<numSectors;i++)
-		{
-			sectors[i]=new Sector();
-			sectors[i]->buf = buf;
-			buf+= sectors[i]->load();
-		}
-	}
+	vector<Section *> lights;
+	vector<Section *> sectors;
 };
 
+Set::Set(Data *data)
+{
+	numSetups = data->GetInt();
+	setups.reserve(numSetups);
+	for(uint32 i = 0; i < numSetups; i++) {
+		setups.push_back(new Setup(data));
+	}
+
+	numLights = data->GetInt();
+	lights.reserve(numLights);
+	for(uint32 i = 0; i < numLights; i++) {
+		lights.push_back(new Light(data));
+	}
+
+	numSectors = data->GetInt();
+	sectors.reserve(numSectors);
+	for(uint32 i = 0; i < numSectors; i++) {
+		sectors.push_back(new Sector(data));
+	}
+}
+string Set::ToString()
+{
+	stringstream ss;
+	// colormaps
+	ss << "section: colormaps" << endl; // we don't have any.
+	// setups
+	ss << "section: setups" << endl;
+	vector<Section*>::iterator it;
+	ss << "\tnumsetups " << setups.size() << endl;
+	for(it = setups.begin(); it != setups.end(); ++it) {
+		ss << (*it)->ToString() << endl << endl;
+	}
+
+	// lights
+	ss << "section: lights" << endl;
+	ss << "\tnumlights 0" << endl;
+	for(it = lights.begin();it!=lights.end();it++) {
+		ss << (*it)->ToString() << endl << endl;
+	}
+	// sectors
+	ss << "section: sectors\n";
+	for(it = sectors.begin(); it != sectors.end();it++) {
+		ss << (*it)->ToString() << endl << endl;
+	}
+	return ss.str();
+}
 int main(int argc, char** argv){
+	if (argc < 2)
+		return 0;
 	char* buf;
 	FILE* f;
-	
+
 	f = fopen(argv[1],"rb");
 	fseek(f,0,SEEK_END);
 	uint32 size = ftell(f);
 	buf = new char[size];
 	fseek(f,0,SEEK_SET);
-	
+
 	int result = fread(buf,1,size,f);
 	assert(result==size);
-	
-	Set* ourSet = new Set(buf);
-	cout << ourSet->MakeVeryLongString();
+	Data *data = new Data(buf);
+	Set* ourSet = new Set(data);
+	delete data;
+	delete buf;
+	cout << ourSet->ToString();
 }
