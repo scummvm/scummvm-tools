a0a7cb6f0cbb1d750341d86e7f69fd3d914bd918
Littleboy <littleboy@users.sourceforge.net>
2011-06-28 05:49:40 -0400

    TOOLS: Update convert_dxa.sh
     - Use ffmpeg instead of wine+bink
     - Handle subfolders when converting smk files
     - Use new scummvm-tools-cli

--- convert_dxa.sh
+++ convert_dxa.sh
@@ -1,37 +1,50 @@
 #! /bin/bash
-# Script for converting Feeble Files video to ScummVM format
+# Script for converting SMK video to ScummVM DXA format
 #
-# modify the paths below
+# Put this batch file into the game directory you want to convert.
+# This script will search for all SMK files in all subtree directories,
+# and encode them as DXA files next to the original files.
 #
-# (c) 2006 ScummVM Team
+# (c) 2006-2011 ScummVM Team
 # (c) 2006 crowley
 #
-# $URL$
-#
-# $Id$
 
-WINE=wine
-VIDEO_PATH=~/games/Feeble/video
-BINK=~/.wine/drive_c/Program\ Files/RADVideo/binkconv.exe
-ENCODE_DXA=~/scummvm/tools/encode_dxa
-FORMAT=vorbis
+echo ScummVM SMK2DXA batch convertor
 
-cd "$VIDEO_PATH"
+###################################################################
+## Edit audio parameter to mp3/vorbis/flac and additional params ##
+###################################################################
 
-for i in *.SMK
+AUDIO=--vorbis
+AUDIO_PARAMS=
+
+###################################################################
+## Default paths for tools and ffmpeg                            ##
+###################################################################
+
+FFMPEG=ffmpeg
+SCUMMVM_TOOLS_CLI=scummvm-tools-cli
+
+for i in $(find $1 -name "*.SMK")
 do
-    in=`basename "$i" .SMK`;
-    mv "$i" "$in.smk";
+	in=`basename "$i" .SMK`;
+	folder=`dirname "$i"`;
+	mv "$i" "$folder/$in.smk";
 done
 
-for i in *.smk
+for i in $(find $1 -name "*.smk")
 do
-    in=`basename "$i" .smk`;
-    if [ ! -f "$in.dxa" ]
-    then
-	$WINE "$BINK" "$i" "$in.png" /n-1 /z1 /#;
-	$WINE "$BINK" "$i" "$in.wav" /v /#;
-	$ENCODE_DXA --$FORMAT "$in.smk";
-	rm *.png *.wav;
-    fi
+	in=`basename "$i" .smk`;
+	folder=`dirname "$i"`;
+	if [ ! -f "$folder/$in.dxa" ]
+	then
+	$FFMPEG -i "$i" -f image2 "$folder/$in.smk%03d.png";
+	$FFMPEG -i "$i" -acodec pcm_u8 "$folder/$in.wav";
+	previous=$cwd
+	cd $folder
+	$SCUMMVM_TOOLS_CLI --tool encode_dxa $AUDIO $AUDIO_PARAMS "$in.smk";
+	rm $in*.png "$in.wav";
+	cd $previous
+	fi
 done
+
