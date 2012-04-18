ad0dfc7ed97be0e4455932782ff910e777974dbc
Andrea Corna <yak.bizzarro+github@corna.info>
2012-04-18 09:31:06 +0200

    MKLAB: Lab files has the same version both in Grim and Emi

--- tools/mklab.cpp
+++ tools/mklab.cpp
@@ -220,12 +220,7 @@ int main(int argc, char **argv) {
 	}
 
 	fwrite("LABN", 1, 4, outfile);
-	if (g_type == GT_GRIM) {
-		fwrite("    ", 1, 4, outfile); //version
-	} else {
-		//EMI expects this as the version number.
-		fwrite("\x00\x00\x01\x00", 1, 4, outfile);
-	}
+	fwrite("\x00\x00\x01\x00", 1, 4, outfile); //version
 	writeUint32(outfile, head.num_entries);
 	writeUint32(outfile, head.string_table_size);
 
