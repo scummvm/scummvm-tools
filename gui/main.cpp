scummvm/scummvm-tools

commit d02758eef0f32c56876cdd39c89a396332a83d4f

TOOLS: Fix crash when compiling with wxWidgets 2.9
The issue occurred on the Intro page when clicking on the Compress,
Extract or Advanced button. The page and therefore the buttons were
destroyed from the event handler of the button that was clicked. While
this work with wxWidgets 2.8 this seems dangerous. And this caused
a crash with wxWidgets 2.9 (at least on mac-cocoa). The page deletion
is now delayed.

gui/main.cpp
@@ -226,6 +226,9 @@ ScummToolsFrame::ScummToolsFrame(const wxString &title, const wxPoint &pos, cons
 	_buttons = new WizardButtons(main, linetext, _configuration);
 	sizer->Add(_buttons, wxSizerFlags().Border().Center().Expand());
 
+	// Delete old panels on idle event
+	Connect(wxEVT_IDLE, wxIdleEventHandler(ScummToolsFrame::destroyOldPanels), NULL, this);
+
 	main->SetSizer(sizer);
 }
 
@@ -235,6 +238,9 @@ ScummToolsFrame::~ScummToolsFrame() {
 	_configuration.save(false);
 
 	for (std::vector<WizardPage *>::iterator iter = _pages.begin(); iter != _pages.end(); ++iter)
+		delete *iter;
+
+	for (std::vector<wxWindow *>::iterator iter = _oldPanels.begin(); iter != _oldPanels.end(); ++iter)
 		delete *iter;
 }
 
@@ -288,9 +294,15 @@ void ScummToolsFrame::switchPage(WizardPage *next, SwitchToPage page) {
 		break;
 	}
 
-	if (oldPanel)
+	if (oldPanel) {
 		// Destroy the old page
-		oldPanel->Destroy();
+		// oldPanel->Destroy();
+		// We have an issue here for the intro page. This is called from the event handler of buttons on the panel
+		// (the Compress, Extract or Advanced button). So we cannot delete the oldPanel now (that would cause a crash).
+		// Instead we hide the panel and it will be deleted later (on the next Idel Event).
+		oldPanel->Hide();
+		_oldPanels.push_back(oldPanel);
+	}
 
 	wxWindow *newPanel = _pages.back()->CreatePanel(_wizardpane);
 
@@ -304,6 +316,13 @@ void ScummToolsFrame::switchPage(WizardPage *next, SwitchToPage page) {
 	_buttons->reset();
 	_buttons->showPrevious(_pages.size() > 1);
 	_buttons->setPage(_pages.back(), newPanel);
+}
+
+void ScummToolsFrame::destroyOldPanels(wxIdleEvent &) {
+	while (!_oldPanels.empty()) {
+		delete _oldPanels.back();
+		_oldPanels.pop_back();
+	}
 }
 
 wxString ScummToolsFrame::GetHelpText() {

gui/main.h
@@ -115,8 +115,6 @@ public:
 	void onMenuAbout(wxCommandEvent &evt);
 	void onMenuExit(wxCommandEvent &evt);
 
-
-
 	/** The state of the wizard so far */
 	Configuration _configuration;
 
@@ -125,10 +123,13 @@ public:
 private:
 	enum SwitchToPage { NextPage, PreviousPage, FirstPage };
 	void switchPage(WizardPage *nextPage, SwitchToPage page);
+    
+	void destroyOldPanels(wxIdleEvent &evt);
 
 	wxPanel *_wizardpane;
 
 	std::vector<WizardPage *> _pages;
+	std::vector<wxWindow *> _oldPanels;
 
 	DECLARE_EVENT_TABLE()
 };

commit 2366007cf50b127bc6aabdbbab09ba66a362975a

TOOLS: Fixed hanged application when running GUI tools
This was a regression introduced by commit d02758e. It caused
onIdle() to be never called and as a result the application didn't show
progress from the tool thread and didn't know either when it had
finished.

gui/main.cpp
@@ -226,9 +226,6 @@ ScummToolsFrame::ScummToolsFrame(const wxString &title, const wxPoint &pos, cons
 	_buttons = new WizardButtons(main, linetext, _configuration);
 	sizer->Add(_buttons, wxSizerFlags().Border().Center().Expand());
 
-	// Delete old panels on idle event
-	Connect(wxEVT_IDLE, wxIdleEventHandler(ScummToolsFrame::destroyOldPanels), NULL, this);
-
 	main->SetSizer(sizer);
 }
 
@@ -318,7 +315,7 @@ void ScummToolsFrame::switchPage(WizardPage *next, SwitchToPage page) {
 	_buttons->setPage(_pages.back(), newPanel);
 }
 
-void ScummToolsFrame::destroyOldPanels(wxIdleEvent &) {
+void ScummToolsFrame::destroyOldPanels() {
 	while (!_oldPanels.empty()) {
 		delete _oldPanels.back();
 		_oldPanels.pop_back();
@@ -375,6 +372,8 @@ void ScummToolsFrame::onMenuExit(wxCommandEvent &evt) {
 }
 
 void ScummToolsFrame::onIdle(wxIdleEvent &evt) {
+	// Delete old panels on idle event
+	destroyOldPanels();
 	if (_pages.back()->onIdle(dynamic_cast<wxPanel *>(_wizardpane->FindWindow(wxT("Wizard Page"))))) {
 		// We want more!
 		evt.RequestMore(true);

gui/main.h
@@ -124,7 +124,7 @@ private:
 	enum SwitchToPage { NextPage, PreviousPage, FirstPage };
 	void switchPage(WizardPage *nextPage, SwitchToPage page);
     
-	void destroyOldPanels(wxIdleEvent &evt);
+	void destroyOldPanels();
 
 	wxPanel *_wizardpane;
 