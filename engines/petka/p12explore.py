dd01092a27d2e408a9dabdd760d631a02777dc2c
Roman Kharin <romiq.kh@gmail.com>
2014-05-12 12:06:37 +0400

    TOOLS: PETKA: Refine outline information

--- engines/petka/p12explore.py
+++ engines/petka/p12explore.py
@@ -11,6 +11,38 @@ import petka
 
 APPNAME = "P1&2 Explorer"
 
+# thanx to http://effbot.org/zone/tkinter-text-hyperlink.htm
+class HyperlinkManager:
+    def __init__(self, text):
+	    self.text = text
+	    self.text.tag_config("hyper", foreground = "blue", underline = 1)
+	    self.text.tag_bind("hyper", "<Enter>", self._enter)
+	    self.text.tag_bind("hyper", "<Leave>", self._leave)
+	    self.text.tag_bind("hyper", "<Button-1>", self._click)
+	    self.reset()
+
+    def reset(self):
+    	self.links = {}
+
+    def add(self, action):
+        # add an action to the manager.  returns tags to use in
+        # associated text widget
+	    tag = "hyper-{}".format(len(self.links))
+	    self.links[tag] = action
+	    return "hyper", tag
+
+    def _enter(self, event):
+    	self.text.config(cursor = "hand2")
+
+    def _leave(self, event):
+    	self.text.config(cursor = "")
+
+    def _click(self, event):
+	    for tag in self.text.tag_names(CURRENT):
+	        if tag[:6] == "hyper-":
+		        self.links[tag]()
+    		    return
+		
 class App(tkinter.Frame):
     def __init__(self, master):
         tkinter.Frame.__init__(self, master)
