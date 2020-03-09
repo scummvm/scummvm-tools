c2ccd50faa7145b76974e81289a14ebce61d91d5
Eugene Sandulenko <sev@scummvm.org>
2020-03-09 15:45:49 +0100

    PRINCE: Added script for automatic generation of prince_translations.dat

---
+++ engines/prince/gen-translations.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cp -v ../../../scummvm/devtools/create_prince/en.po .
+perl po-parse.pl en en.po
+../../scummvm-tools-cli --tool pack_prince .
+cp prince_translation.dat ../../../scummvm/dists/engine-data/
