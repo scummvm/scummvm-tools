202825de54760d2c0f4a4014e15a347cf2de33e2
Eugene Sandulenko <sev@scummvm.org>
2020-03-11 01:12:28 +0100

    PRINCE: Fix inventory packing

--- engines/prince/pack_prince.cpp
+++ engines/prince/pack_prince.cpp
@@ -232,7 +232,7 @@ void PackPrince::packInvTxt() {
 	}
 
 	// Skip comments until first inventory item nr
-	while ((c = _databank.readByte()) != '0');
+	while ((c = _databank.readByte()) != '1');
 
 	// Skip first dot and space
 	_databank.readByte();
